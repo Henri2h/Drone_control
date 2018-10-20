7575865c017bb7ca152f1bfafec22f530bcd120d
Henri2h <henri.carnot@hotmail.fr>
2018-10-20 12:11:34 +0200

    Update src/Filter.cpp

--- src/Filter.cpp
+++ src/Filter.cpp
@@ -4,7 +4,10 @@ class Filter{
     float complementaryFilter(float a, float b, float coef_a, float coef_b){
         return a * coef_a + b * coef_b;
     }
-    float low_pass_filter(float i, float e){
-        return 0.0;
+    float low_pass_filter(float Vsn, float Ven){
+        // according to shannon criteria, 
+        float w0Te = 3.14/10; // produit de w0Te car w0 = 2*pi*Te
+        float vsn = (Ven * w0Te + Vsn)/ (w0Te + 1);
+        return vsn;
     }
 };
