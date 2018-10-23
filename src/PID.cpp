0d7fdae97081fd5a6e5dd3d8d8a85305162ab88b
Henri2h <Henri2h@users.noreply.github.com>
2018-10-23 22:29:43 +0200

    added integralWindup and K setting

--- src/PID.cpp
+++ src/PID.cpp
@@ -19,8 +19,30 @@ class PID
     float Ki[3] = {0, 0, 0};
     float last_time = 0;
 
-    void integralWindup() {}
-    void filter() {}
+    int max_integral = 500;
+    void integralWindup()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            if (error_i[i] > max_integral)
+            {
+                error_i[i] = max_integral;
+            }
+            else if (error_i[i] < -max_integral)
+            {
+                error_i[i] = -max_integral;
+            }
+        }
+    }
+
+    float w0Te = 3.14 * 2; // produit de w0Te car w0 = 2*pi*Te
+    void doFilter()
+    {
+        for (size_t i = 0; i < 3; i++)
+        {
+            error_d[i] = (error_d_last[i] * w0Te + error_d[i]) / (w0Te + 1);
+        }
+    }
 
     float mapValue(float in_val, float in_min, float in_max, float out_min, float out_max)
     {
@@ -34,15 +56,33 @@ class PID
         cout << "[ PID ] : initialized\n";
     }
 
-    int *getPID(int output[], int commands[], float feedbacks[], float time_in)
+    void setK(int kp, int kd, int ki)
+    {
+        
+        Kp[0] = mapValue(kp, 922, 2077, 0, 4);
+        Kd[0] = mapValue(kd, 922, 2077, 0, 1);
+        Ki[0] = mapValue(ki, 921, 2077, 0, 1);
+        cout << "kp : " << Kp[0] << " " << Kd[0] << " " << Ki[0] << "\n";
+
+        //  Kp[1] = mapValue(kp, 1000, 2000, 0, 100);
+        //  Kd[1] = mapValue(kd, 1000, 2000, 0, 100);
+        //  Ki[1] = mapValue(ki, 1000, 2000, 0, 100);
+    }
+
+    int *getPID(int output[], int debug[], int commands[], float feedbacks[], float dt)
     {
         for (int i = 0; i < 3; i++)
         {
-            error_p[i] = commands[i] - feedbacks[i];
+            error_p[i] = mapValue(commands[i], 1075, 1920, -30, 30) - feedbacks[i];
+            error_d[i] = (error_p[i] - error_p_last[i]) / dt;
+            error_i[i] += error_p[i] * dt;
+            // cout << "i : " << i << " : " << mapValue(commands[i], 1000, 2000, -30, 30) << " error : " << error_p[i] << " ";
         }
 
+        //        cout << "\n";
+
         integralWindup();
-        filter();
+        doFilter();
 
         // save
         for (int i = 0; i < 3; i++)
@@ -51,12 +91,15 @@ class PID
             error_d_last[i] = error_d[i];
             error_i_last[i] = error_i[i];
         }
-        last_time = time_in;
 
         for (int i = 0; i < 3; i++)
         {
             output[i] = (Kp[i] * error_p[i]) + (Kd[i] * error_d[i]) + (Ki[i] * error_i[i]);
         }
+
+        debug[0] = Kp[0] * error_p[0];
+        debug[1] = Kd[0] * error_d[0];
+        debug[2] = Ki[0] * error_i[0];
 
         return output;
     }
