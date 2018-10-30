6707c4136c2f5566a729844c7f6a21299216ca1d
Henri2h <Henri2h@users.noreply.github.com>
2018-10-30 18:52:25 +0100

    New  work on stabilisation

---
+++ src/Stabilisation.cpp
@@ -0,0 +1,9 @@
+class Stabilisation{
+    private:
+    public:
+    Stabilisation(){}
+
+    float * Stabilize(float *motor_command ,float *ang, float *gyr){
+        return motor_command
+    }
+}
