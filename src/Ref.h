43a24246bfbcfd7ec5f3099b0d3f8d99f3dc51af
Henri2h <Henri2h@users.noreply.github.com>
2018-11-10 12:03:41 +0100

    Reference to make things easier

---
+++ src/Ref.h
@@ -0,0 +1,20 @@
+#define cmd_throttle 2
+#define cmd_pitch 1
+#define cmd_roll 0
+#define cmd_yaw 3
+#define cmd_arming 8
+#define flight_mode 5
+#define cmd_kp 6
+#define cmd_kd 4
+#define cmd_ki 7
+
+#define pos_x 0
+#define pos_y 1
+#define pos_z 2
+
+#define pid_pitch 1
+#define pid_roll 0
+#define pid_yaw 2
+
+#define grav 9.81
+#define pi 3.14159
