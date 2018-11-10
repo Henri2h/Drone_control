0842f6a24a7ce99961978aa1617fbb3941a879b3
Henri2h <Henri2h@users.noreply.github.com>
2018-11-10 12:01:38 +0100

    New pid

---
+++ src/Stab/PID_single_axis.cpp
@@ -0,0 +1,103 @@
+#include <unistd.h>
+#include <memory>
+#include <iostream>
+using namespace std;
+
+class PID_Single_Axis
+{
+  private:
+    float error_p = 0;
+    float error_d = 0;
+    float error_i = 0;
+
+    float error_p_last = 0;
+    float error_d_last = 0;
+    float error_i_last = 0;
+
+    float Kp = 0;
+    float Kd = 0;
+    float Ki = 0;
+    float last_time = 0;
+
+    int max_integral = 100;
+    void integralWindup()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            if (error_i > max_integral)
+            {
+                error_i = max_integral;
+            }
+            else if (error_i < -max_integral)
+            {
+                error_i = -max_integral;
+            }
+        }
+    }
+
+    float w0Te = 20; // produit de w0Te car w0 = 2*pi*Te
+    void doFilter()
+    {
+        error_d = (error_d_last * w0Te + error_d) / (w0Te + 1);
+
+        if (error_d > max_integral)
+        {
+            error_d = max_integral;
+        }
+        else if (error_d < -max_integral)
+        {
+            error_d = -max_integral;
+        }
+    }
+
+    float mapValue(float in_val, float in_min, float in_max, float out_min, float out_max)
+    {
+        float out_val = (out_max - out_min) / (in_max - in_min) * (in_val - in_min) + out_min;
+        return out_val;
+    }
+
+  public:
+    PID_Single_Axis()
+    {
+        cout << "[ PID Single Axis ] : Initialized\n";
+    }
+
+    void setK(int kp, int kd, int ki)
+    {
+
+        Kp = mapValue(kp, 922, 2077, 0, 4);
+        Kd = mapValue(kd, 922, 2077, 0, 1);
+        Ki = mapValue(ki, 921, 2077, 0, 1);
+
+        //  Kp[1] = mapValue(kp, 1000, 2000, 0, 100);
+        //  Kd[1] = mapValue(kd, 1000, 2000, 0, 100);
+        //  Ki[1] = mapValue(ki, 1000, 2000, 0, 100);
+    }
+
+    float update(float error, float dt)
+    {
+        float output = 0;
+
+        error_p = error;
+        // error_d[i] = (error_p[i] - error_p_last[i]) / dt;
+        error_d = (-error_p_last) / dt;
+        error_i += error_p * dt;
+        // cout << "i : " << i << " : " << mapValue(commands[i], 1000, 2000, -30, 30) << " error : " << error_p[i] << " ";
+
+        //        cout << "\n";
+
+        integralWindup();
+        doFilter();
+
+        error_p_last = error_p;
+        error_d_last = error_d;
+        error_i_last = error_i;
+
+        output = (Kp * error_p) + (Kd * error_d) + (Ki * error_i);
+        return output;
+    }
+    void displayK()
+    {
+        cout << "kp : " << Kp << " " << Kd << " " << Ki << "\n";
+    }
+};
