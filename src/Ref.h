ff233ad3bad1176ffc57a5af79539a4f5dbede31
Henri2h <henri.carnot@hotmail.fr>
2018-12-29 11:45:22 +0100

    wrong reference correction + added status

--- src/Ref.h
+++ src/Ref.h
@@ -12,9 +12,20 @@
 #define pos_y 1
 #define pos_z 2
 
-#define pid_pitch 1
-#define pid_roll 0
+#define pid_pitch 0
+#define pid_roll 1
 #define pid_yaw 2
+
+#define rates_pitch 0
+#define rates_roll 1
+#define rates_yaw 2
+
+
+#define status_Saving 0
+#define statuse_DisplayGains 1
+
+#define order_Saving 0
+#define orders_DisplayGains 1
 
 #define grav 9.81
 #define pi 3.14159
