68b02a28ef792c9ed8871c76bf9eabef2f0072f3
Henri2h <henri.carnot@hotmail.fr>
2018-12-29 11:41:56 +0100

    Added a way to make the led blink

--- src/LEDManager.cpp
+++ src/LEDManager.cpp
@@ -7,7 +7,8 @@
 class LEDManager
 {
   private:
-    std::unique_ptr<Led> get_led(){
+    std::unique_ptr<Led> get_led()
+    {
         if (get_navio_version() == NAVIO2)
         {
             auto ptr = std::unique_ptr<Led>{new Led_Navio2()};
@@ -19,38 +20,74 @@ class LEDManager
             return ptr;
         }
     }
-    std::unique_ptr<Led> led = get_led(); // save led 
+    std::unique_ptr<Led> led = get_led(); // save led
+ 
+    int rate = 300;
+    int mode = 0;
+    int updateIter = 0;
 
   public:
-    LEDManager(){
+    LEDManager()
+    {
         if (!led->initialize())
         {
             std::cout << "[ LEDManager ] : Cannot initialize ...\n";
             throw EXIT_FAILURE;
         }
     }
-
-    void setOK()
+    /* mode :
+     0 : KO or black
+     1 : OK
+     2 : Arming
+     3 : Armed
+    */
+    void update()
     {
-        led->setColor(Colors::Green);
+        if (mode == 1 && updateIter == 1)
+        {
+            led->setColor(Colors::Green);
+        }
+        if (mode == 2 && updateIter == 1)
+        {
+            led->setColor(Colors::Cyan);
+        }
+        else if ((mode == 1 || mode == 2) && updateIter == rate)
+        {
+            led->setColor(Colors::Black);
+        }
+        else if (updateIter > 2*rate)
+        {
+            updateIter = 0;
+        }
+
+        updateIter++;
     }
 
     void setKO()
     {
+        mode = 0;
         led->setColor(Colors::Red);
     }
+
+    void setOK()
+    {
+        mode = 1; // blinking state
+    }
+
     void setArmed()
     {
+        mode = 3;
         led->setColor(Colors::Blue);
     }
 
-    void setBlack(){
+    void setBlack()
+    {
+        mode = 0;
         led->setColor(Colors::Black);
     }
 
     void setArming()
     {
-        led->setColor(Colors::Cyan);
+        mode = 2;  // blinking state
     }
-
 };
