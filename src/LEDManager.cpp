eea2b06881296a8021363344f2a922df44f67091
Henri2h <henri.carnot@hotmail.fr>
2019-01-02 00:28:14 +0100

    enhanced led manager

--- src/LEDManager.cpp
+++ src/LEDManager.cpp
@@ -21,9 +21,17 @@ class LEDManager
         }
     }
     std::unique_ptr<Led> led = get_led(); // save led
- 
+
     int rate = 300;
     int mode = 0;
+
+  /* mode :
+     0 : KO or black
+     1 : OK
+     2 : Arming
+     3 : Armed
+    */
+
     int updateIter = 0;
 
   public:
@@ -34,33 +42,32 @@ class LEDManager
             std::cout << "[ LEDManager ] : Cannot initialize ...\n";
             throw EXIT_FAILURE;
         }
+        setKO();
+        std::cout << "[ LEDManager ] : Starting ...\n";
     }
-    /* mode :
-     0 : KO or black
-     1 : OK
-     2 : Arming
-     3 : Armed
-    */
+  
     void update()
     {
-        if (mode == 1 && updateIter == 1)
+        if (mode == 1 && updateIter == 0) // mode ok and start of the loop
         {
             led->setColor(Colors::Green);
         }
-        if (mode == 2 && updateIter == 1)
+
+        if (mode == 2 && updateIter == 0) // mode arming and start of the loop
         {
             led->setColor(Colors::Cyan);
         }
-        else if ((mode == 1 || mode == 2) && updateIter == rate)
+        if ((mode == 1 || mode == 2) && updateIter == rate)
         {
             led->setColor(Colors::Black);
         }
-        else if (updateIter > 2*rate)
+
+        updateIter++;
+
+        if (updateIter > 2 * rate)
         {
             updateIter = 0;
         }
-
-        updateIter++;
     }
 
     void setKO()
@@ -88,6 +95,12 @@ class LEDManager
 
     void setArming()
     {
-        mode = 2;  // blinking state
+        mode = 2; // blinking state
+    }
+    
+    void backToPrevious(){ // prevent deleting KO state
+        if(mode > 0){
+            mode = 1;
+        }
     }
 };
