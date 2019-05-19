942adacc94c2892225e38e2969c389b0c75517f4
henri2h <henri.carnot@hotmail.fr>
2019-05-19 16:25:07 +0200

    Enhanced filter

--- src/Filter.cpp
+++ src/Filter.cpp
@@ -1,16 +1,22 @@
 
 class Filter
 {
+    float pi = 3.14;
   public:
     float complementaryFilter(float a, float b, float coef_a, float coef_b)
     {
         return a * coef_a + b * coef_b;
     }
-    float low_pass_filter(float Vsn, float Ven)
+
+    // e : entrée
+    // s : sortie à t-dt
+    float low_pass_filter(float s, float e)
     {
         // according to shannon criteria,
-        float w0Te = 3.14 / 10; // produit de w0Te car w0 = 2*pi*Te
-        float vsn = (Ven * w0Te + Vsn) / (w0Te + 1);
+        float rapport = pi / 10; // produit de w0Te car w0 = 2*pi*Te car10T = dt*
+        rapport = 20; // w0Te = 10
+
+        float vsn = (e * rapport + s) / (rapport + 1);
         return vsn;
     }
 };
