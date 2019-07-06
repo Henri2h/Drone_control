8263d2f6dbe2c49fb9c2c58c421430954a40c630
henri2h <henri.carnot@hotmail.fr>
2019-07-06 12:41:54 +0200

    Work on remote

--- src/Remote.cpp
+++ src/Remote.cpp
@@ -50,12 +50,10 @@ private:
 		{
 			char charray[14]; // warning : max 14 characters !!!!!
 			//cout << "i : " << i << " : " << dt->status[i] << " ";
-
 			char const *val = to_string(dt->status[i]).c_str();
 			// sending data
 			ws->send(val, strlen(val), opCode);
 		}
-		cout << "\n";
 		ws->send(time_str, strlen(time_str), opCode);
 	}
 
@@ -106,9 +104,9 @@ private:
 		char const *comp_str_2 = to_string(dt->ang[2]).c_str(); // comp ang
 
 		// pid
-		char const *pid_str_0 = to_string(dt->pid_out[0]).c_str();
-		char const *pid_str_1 = to_string(dt->pid_out[1]).c_str();
-		char const *pid_str_2 = to_string(dt->pid_out[2]).c_str();
+		char const *pid_str_0 = to_string(dt->controller_gains_rates[0]).c_str();
+		char const *pid_str_1 = to_string(dt->controller_gains_rates[1]).c_str();
+		char const *pid_str_2 = to_string(dt->controller_gains_rates[2]).c_str();
 
 		// sending data
 		ws->send(time_str, strlen(time_str), opCode); // time
@@ -140,6 +138,17 @@ private:
 		ws->send(pid_str_2, strlen(pid_str_2), opCode);
 	}
 
+	static std::vector<std::string> split(const std::string &s, char delimiter)
+	{
+		std::vector<std::string> tokens;
+		std::string token;
+		std::istringstream tokenStream(s);
+		while (std::getline(tokenStream, token, delimiter))
+		{
+			tokens.push_back(token);
+		}
+		return tokens;
+	}
 
 	static void start_remote(Data *data_i, float *time_now_in)
 	{
@@ -159,87 +168,181 @@ private:
 				}
 				string r = string(os.str());
 
-				cout << "On Message : " << r << "\n";
-				if if (r.compare("#FStatus") == 0)
-				{
-					sendFStatus(ws, dt, time_pointer, opCode);
-				}
-				else if (r.compare("#Status") == 0)
-				{
-					sendDataL(ws, dt, time_pointer, status_length, opCode);
-				}
-				else if (r.compare("#AngComp") == 0)
-				{
-					sendData(ws, dt->ang, time_pointer, opCode);
-				}
+				std::vector<std::string> L_r = split(r, ' ');
 
-				else if (r.compare("#PIDErrP") == 0)
+				if (L_r.size() == 1)
 				{
-					sendData(ws, dt->pid_debug, time_pointer, opCode);
-				}
-				else if (r.compare("#PID") == 0)
-				{
-					sendData(ws, dt->pid_out, time_pointer, opCode);
-				}
+					if (L_r[0].compare("#ExpAbort") == 0)
+					{
+						dt->status[status_experience_mode] = 0;
+						FileManagement::Log("Remote", "Experience aborted");
+						dt->orders[0] = 0; // stop saving
+						ws->send("OK", 2, opCode);
+					}
+					else if (L_r[0].compare("#ExpStart") == 0)
+					{
+						FileManagement::Log("Remote", "Experience started");
+						dt->status[status_experience_mode] = 1;
+						ws->send("OK", 2, opCode);
+					}
 
-				else if (r.compare("#Acc") == 0)
-				{
-					sendData(ws, dt->acceleration, time_pointer, opCode);
-				}
-				else if (r.compare("#Gyr") == 0)
-				{
-					sendData(ws, dt->rates, time_pointer, opCode);
-				}
+					else if (L_r[0].compare("#FStatus") == 0)
+					{
+						sendFStatus(ws, dt, time_pointer, opCode);
+					}
+					else if (L_r[0].compare("#Status") == 0)
+					{
+						sendDataL(ws, dt, time_pointer, status_length, opCode);
+					}
 
-				else if (r.compare("#ListFiles") == 0)
-				{
-					// nothing
-					std::cout << "#ListFiles\n";
-				}
-				else if (r.compare("#OrdRecordOn") == 0)
-				{
-					// nothing
-					std::cout << "OrdRecordOn\n";
-					dt->orders[0] = 1;
-					ws->send("OK", 2, opCode);
-				}
-				else if (r.compare("#OrdRecordOff") == 0)
-				{
-					// nothing
-					std::cout << "OrdRecordOff\n";
-					dt->orders[0] = 0;
-					ws->send("OK", 2, opCode);
-				}
+					else if (r.compare("#OrdRecordOn") == 0)
+					{
+						// nothing
+						std::cout << "OrdRecordOn\n";
+						dt->orders[0] = 1;
+						ws->send("OK", 2, opCode);
+					}
+					else if (r.compare("#OrdRecordOff") == 0)
+					{
+						// nothing
+						std::cout << "OrdRecordOff\n";
+						dt->orders[0] = 0;
+						ws->send("OK", 2, opCode);
+					}
 
-				// DK : display gains
-				else if (r.compare("#OrdDKOn") == 0)
+					// DK : display gains
+					else if (r.compare("#OrdDKOn") == 0)
+					{
+						// nothing
+						dt->orders[1] = 1;
+						ws->send("OK", 2, opCode);
+					}
+					else if (r.compare("#OrdDKOff") == 0)
+					{
+						// nothing
+						dt->orders[orders_DisplayGains] = 0;
+						ws->send("OK", 2, opCode);
+					}
+
+					// File Management :
+					else if (r.compare("#FL"))
+					{ // FL : FileList
+						std::string strlist = FileManagement::listDir("/home/pi/log");
+						char const *data = strlist.c_str();
+						ws->send(data, strlen(data), opCode);
+					}
+
+					else if (r.compare("#FS"))
+					{ // FL : FileSend
+					}
+
+					else if (r.compare("#AngComp") == 0)
+					{
+						sendData(ws, dt->ang, time_pointer, opCode);
+					}
+
+					else if (r.compare("#PIDErrP") == 0)
+					{
+						sendData(ws, dt->pid_debug, time_pointer, opCode);
+					}
+					else if (r.compare("#PID") == 0)
+					{
+						sendData(ws, dt->pid_out, time_pointer, opCode);
+					}
+
+					else if (r.compare("#Acc") == 0)
+					{
+						sendData(ws, dt->acceleration, time_pointer, opCode);
+					}
+					else if (r.compare("#Gyr") == 0)
+					{
+						sendData(ws, dt->rates, time_pointer, opCode);
+					}
+
+					else if (r.compare("#ListFiles") == 0)
+					{
+						// nothing
+						std::cout << "#ListFiles\n";
+					}
+
+					else
+					{
+						FileManagement::Log("Remote", "command unknown : |" + r + "|");
+						ws->send("KO", 2, opCode);
+					}
+				}
+				else if (L_r.size() > 1)
 				{
-					// nothing
-					dt->orders[1] = 1;
-					ws->send("OK", 2, opCode);
-				}
-				else if (r.compare("#OrdDKOff") == 0)
-				{
-					// nothing
-					dt->orders[orders_DisplayGains] = 0;
-					ws->send("OK", 2, opCode);
-				}
 
-				// File Management :
-				else if (r.compare("#FL"))
-				{ // FL : FileList
-					std::string strlist = FileManagement::listDir("/home/pi/log");
-					char const *data = strlist.c_str();
-					ws->send(data, strlen(data), opCode);
+					if (L_r[0].compare("#setFilter") == 0)
+					{ // set filter
+						if (L_r[1].compare("NONE") == 0)
+						{
+							dt->stabilisation_mode = IMU_Filter_usage_none;
+							dt->status[status_filter_mode] = IMU_Filter_usage_none;
+							FileManagement::Log("Remote", "Filter mode none");
+							ws->send("OK", 2, opCode);
+						}
+						else if (L_r[1].compare("ACC") == 0)
+						{
+							dt->stabilisation_mode = IMU_Filter_usage_acc;
+							dt->status[status_filter_mode] = IMU_Filter_usage_acc;
+							FileManagement::Log("Remote", "Filter mode acc");
+							ws->send("OK", 2, opCode);
+						}
+						else if (L_r[1].compare("GYR") == 0)
+						{
+							dt->stabilisation_mode = IMU_Filter_usage_gyr;
+							dt->status[status_filter_mode] = IMU_Filter_usage_gyr;
+							FileManagement::Log("Remote", "Filter mode gyr");
+							ws->send("OK", 2, opCode);
+						}
+						else if (L_r[1].compare("BOTH") == 0)
+						{
+							dt->stabilisation_mode = IMU_Filter_usage_both;
+							dt->status[status_filter_mode] = IMU_Filter_usage_both;
+							FileManagement::Log("Remote", "Filter both");
+							ws->send("OK", 2, opCode);
+						}
+						else
+						{
+							FileManagement::Log("Remote", "setFilter : parameter unknown : |" +  L_r[0] + "|" +  L_r[1] +"|");
+							ws->send("KO", 2, opCode);
+						}
+					}
+					else if (L_r[0].compare("#OrdRecord") == 0)
+					{
+						if (L_r[1].compare("OFF") == 0)
+						{
+							// nothing
+							std::cout << "OrdRecordOff\n";
+							dt->orders[0] = 0;
+							ws->send("OK", 2, opCode);
+						}
+						else if (L_r[1].compare("ON") == 0)
+						{
+							// nothing
+							std::cout << "OrdRecordOn\n";
+							dt->orders[0] = 1;
+							ws->send("OK", 2, opCode);
+						}
+					}
+					else if(L_r[0].compare("#SetGainsRate") == 0){
+						cout << L_r[1]  << "\n";
+						dt->controller_gains_rates[pid_roll] = std::stof(L_r[1]);
+						cout <<"gains : " << dt->controller_gains_rates[pid_roll] << "\n";
+						ws->send("OK", 2, opCode);
+					}
+					else
+					{
+						FileManagement::Log("Remote", "command unknown : |" +  L_r[0] + "|" +  L_r[1] +"|");
+						ws->send("KO", 2, opCode);
+					}
 				}
-
-				else if (r.compare("#FS"))
-				{ // FL : FileSend
-				}
-
 				else
 				{
-					FileManagement::Log("Remote", "command unknown");
+					FileManagement::Log("Remote", "command unknown : |" + r + "|");
+					cout << "L_r size : " <<L_r.size() << "\n";
 					ws->send("KO", 2, opCode);
 				}
 			}
